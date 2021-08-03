b951216d1341ce72e481bdde27b4f0a00ccc9163
Aliya Mullina <aliya.mullina@ya.ru>
2021-08-03 15:01:34 +0300

    Описаны пользовательские исключения

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,20 @@
 class InputParameterVerificationError(Exception):
-    pass
+    """Исключение для input_validation()."""
+
+    def __init__(self, message='Ошибка валидации параметра'):
+        self.message = message
+        super().__init__(message)
+
+    def __str__(self):
+        return self.message
 
 
 class ResultVerificationError(Exception):
-    pass
+    """Исключение для result_validation()."""
+
+    def __init__(self, message='Ошибка валидации результата'):
+        self.message = message
+        super().__init__(message)
+
+    def __str__(self):
+        return self.message
