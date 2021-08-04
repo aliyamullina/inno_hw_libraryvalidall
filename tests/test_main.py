79a862f4c6079824ffb9d1e8f11a61c4d937f938
Aliya Mullina <aliya.mullina@ya.ru>
2021-08-04 16:20:01 +0300

    Скелет обязательных проверок по условию задачи

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,13 +2,27 @@ import pytest
 
 
 class TestValidAll(pytest.TestCase):
-    """Попробовать разные json в main."""
-    # Не проходит регулярку:
-    # with open('not_regex_sites.json', 'r', encoding='utf-8') as file_json:
-    # Не проходит валидацию:
-    # with open('invalid_sites.json', 'r', encoding='utf-8') as file_json:
+    """Тестирование декоратора."""
 
-    def test_what(self):
+    def test_validate_json_with_schema(self):
+        """Валидация входного json-параметра."""
+        # Не проходит валидацию:
+        # with open('invalid_sites.json', 'r', encoding='utf-8') as file_json:
+        pass
+
+    def test_check_ip_address_with_regex(self):
+        """Валидация строки по регулярному выражению."""
+        # Не проходит регулярку:
+        # with open('not_regex_sites.json', 'r', encoding='utf-8') as file_json:
+        pass
+
+    def test_valid_all_validation(self):
+        """Проверка валидации возвращаемого результата."""
+        pass
+
+    def test_valid_all_exception(self):
+        """Проверка что при определенных условиях действительно поднимаются исключения
+        InputParameterVerificationError, ResultVerificationError."""
         pass
 
 
